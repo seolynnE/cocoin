b0eaa472c4c064cd0cd6641ed7b0b2f26baf58a4
seolynnE <tjfls2004@gmail.com>
2023-12-27 13:31:56 +0900

    메인페이지로 바로 안가는 현상 해결

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as MyRouter, Routes, Route } from "react-router-dom";
 import Coins from "./routes/Coins";
 import Coin from "./routes/Coin";
 import Chart from "./routes/Chart";
@@ -7,16 +7,16 @@ import Header from "./routes/Header";
 
 function Router() {
   return (
-    <BrowserRouter>
+    <MyRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Coins />} />
-        <Route path="/:coinId/*" element={<Coin />}>
+        <Route path="/cocoin" element={<Coins />} />
+        <Route path="/cocoin/:coinId/*" element={<Coin />}>
           <Route path="chart" element={<Chart />} />
           <Route path="price" element={<Price />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </MyRouter>
   );
 }
 
